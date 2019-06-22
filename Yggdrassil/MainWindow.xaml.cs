821e1918f4617311acff9b3a2d30596bc5ef23ac
Jeroen P. Broks <facebook@tbbs.nl>
2019-06-22 11:31:58 +0200

    vUpdate

--- Yggdrassil/MainWindow.xaml.cs
+++ Yggdrassil/MainWindow.xaml.cs
@@ -21,8 +21,9 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 19.06.21
+// Version: 19.06.22
 // EndLic
+
 
 
 
@@ -62,7 +63,7 @@ namespace Yggdrassil {
         public MainWindow() {
             Debug.WriteLine("Loading main window");
             MKL.Lic    ("Yggdrassil - MainWindow.xaml.cs","GNU General Public License 3");
-            MKL.Version("Yggdrassil - MainWindow.xaml.cs","19.06.21");
+            MKL.Version("Yggdrassil - MainWindow.xaml.cs","19.06.22");
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             Title = $"Yggdrassil version {MKL.Newest}";
@@ -599,6 +600,7 @@ namespace Yggdrassil {
         }
     }
 }
+
 
 
 
--- Yggdrassil/Needed/XSource/Wiki.cs
+++ Yggdrassil/Needed/XSource/Wiki.cs
@@ -21,8 +21,9 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 19.06.21
+// Version: 19.06.22
 // EndLic
+
 
 
 using System;
@@ -71,5 +72,6 @@ namespace Yggdrassil.Needed.XSource {
         }
     }
 }
+
 
 
