ab69aff5bc930d741c5b1bb35da63389d7cec711
Jeroen P. Broks <facebook@tbbs.nl>
2019-06-22 13:33:44 +0200

    Fix: Value parameter forgotten

--- Yggdrassil/Needed/JavaScript/WikiScript.js
+++ Yggdrassil/Needed/JavaScript/WikiScript.js
@@ -22,11 +22,11 @@
 // This script just needs to be attached to every Wiki Page
 
 const Var2Script = {}
-const Var2ScriptCall = (vcall) => {
+const Var2ScriptCall = (vcall,value) => {
 	if (!Var2Script[vcall]) {
 		alert(`There is no script for Wiki Variable ${vcall}!\n\nPlease notify the webmaster of this error!`)
 		return Undefined
 	}
-	return Var2Script[vcall](vcall)
+	return Var2Script[vcall](vcall,value)
 }
 
